c9a4ec4a9e397d1f942d834bd2017d715c5dbed3
Fily1212 <Fily1212>
2025-05-04 16:39:14 +0200

    order-service

---
+++ order-service/src/test/java/it/unito/orderservice/OrderServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package it.unito.orderservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
